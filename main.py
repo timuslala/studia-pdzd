a72d19e05491cb4b1fecd55e33430fca30a095bc
Przemysław Tomala <timuslala@gmail.com>
2026-04-21 15:41:30 +0200

    fullfillement of all requirements

--- main.py
+++ main.py
@@ -1,39 +1,66 @@
 import requests
 import pandas as pd
 import time
+from time import datetime
+import os 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+
+# --- RUN ID ---
+RUN_ID = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+LOG_FILE = os.path.join(LOG_DIR, f"pipeline_{RUN_ID}.log")
+def log(message):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    full_msg = f"[{timestamp}] {message}"
+
+    print(full_msg)
+
+    with open(LOG_FILE, "a", encoding="utf-8") as f:
+        f.write(full_msg + "\n")
+
 
 def download_csv(url, csv_suffix):
-    page = 1
-    all_data = []
+    try:
+        page = 1
+        all_data = []
 
-    while True:
-        params = {
-            "page": page,
-            "limit": 10000,
-            "coo_all": "true",
-            "coa_all": "true"
-        }
+        while True:
+            params = {
+                "page": page,
+                "limit": 10000,
+                "coo_all": "true",
+                "coa_all": "true"
+            }
 
-        r = requests.get(url, params=params)
-        print("Status:", r.status_code)
-        print("Content:", r.text[:200])  # first 200 chars
-        data = r.json()
+            r = requests.get(url, params=params)
+            data = r.json()
 
-        items = data.get("items", [])
+            items = data.get("items", [])
 
-        if not items:
-            break
+            if not items:
+                break
 
-        all_data.extend(items)
+            all_data.extend(items)
 
-        print(f"Strona {page}, rekordów: {len(items)}")
+            log(f"Strona {page}, rekordów: {len(items)}")
 
-        page += 1
+            page += 1
 
-    df = pd.DataFrame(all_data)
-    df.to_csv(f"unhcr_{csv_suffix}.csv", index=False)
-
-    print("Zapisano:", len(df), "rekordów")
+        df = pd.DataFrame(all_data)
+        df.to_csv(f"unhcr_{csv_suffix}.csv", index=False)
+        
+        filename = f"unhcr_{csv_suffix}.csv"
+        file_size = os.path.getsize(filename)
+        log(f"Zapisano {filename} ({file_size} bajtów): {len(df)} rekordów")
+        
+        os.system(f"hdfs dfs -mkdir -p /unhcr/")
+        os.system(f"hdfs dfs -put {filename} /unhcr/")
+        os.system(f"hdfs dfs -setrep -w 3 /unhcr/{filename}")
+        log(f"Wgrano na HDFS z 3 replikami plik: {filename}")
+    except Exception as e:
+        log(f"Wystąpił błąd: {e}")
 
 if __name__=="__main__":
     urlpopulation = "https://api.unhcr.org/population/v1/population/"
