17b2ad3c52d18f8b59d7896879de49bc2bd7389f
Przemysław Tomala <timuslala@gmail.com>
2026-04-21 15:43:01 +0200

    fix logdir not existing

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 LOG_DIR = os.path.join(BASE_DIR, "logs")
-
+os.makedirs(LOG_DIR, exist_ok=True)
 # --- RUN ID ---
 RUN_ID = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
